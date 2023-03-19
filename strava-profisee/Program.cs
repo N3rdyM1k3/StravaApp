01fc0a18f567dc0593d9a2e15a7a263a38b32493
Mike Peacock <MAPeacock86@gmail.com>
2023-03-19 14:39:10 -0400

    outline of auth

--- strava-profisee/Program.cs
+++ strava-profisee/Program.cs
@@ -2,6 +2,15 @@ using StravaProfisee;
 
 var app = WebApplication.Create(args);
 // var stravaClient = new StravaClient();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddStrava(options =>
+{
+    options.ClientId = "";
+    options.ClientSecret = "";
+}
+);
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGet("/", () => "Hello World");
 
 app.Run();
