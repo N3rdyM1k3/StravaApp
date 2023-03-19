64222a1ce2b6d9dd657077063d507e838a17af90
Mike Peacock <MAPeacock86@gmail.com>
2023-03-19 14:48:12 -0400

    read secrets

--- strava-profisee/Program.cs
+++ strava-profisee/Program.cs
@@ -1,16 +1,35 @@
 using StravaProfisee;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
+SecretClientOptions options = new SecretClientOptions()
+    {
+        Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+    };
+var client = new SecretClient(new Uri("https://profisee-strava-keyvault.vault.azure.net/"), new DefaultAzureCredential(),options);
+
+KeyVaultSecret secret = client.GetSecret("ClientId");
+
+string secretValue = secret.Value;
 
 var app = WebApplication.Create(args);
 // var stravaClient = new StravaClient();
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAuthentication().AddStrava(options =>
-{
-    options.ClientId = "";
-    options.ClientSecret = "";
-}
-);
-app.UseAuthentication();
-app.UseAuthorization();
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddAuthentication().AddStrava(options =>
+// {
+//     options.ClientId = "";
+//     options.ClientSecret = "";
+// }
+// );
+// app.UseAuthentication();
+// app.UseAuthorization();
 app.MapGet("/", () => "Hello World");
-
+app.MapGet("/cid", () => secretValue);
 app.Run();
